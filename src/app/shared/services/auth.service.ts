87ad8f1ae6557c7632be24b76760c764034c23c8
Mateus Oliveira Paula <omateuspaula@gmail.com>
2022-11-29 15:10:58 -0300

    set user na service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,9 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(values: {registration: string, password: string}){
-    return this.http.post<User>(environment.GEDUC_API + '/v1/user/auth',  values);
+    return this.http.post<User>(environment.GEDUC_API + '/v1/user/auth',  values).pipe(tap((response) => {
+      sessionStorage.setItem('user', JSON.stringify(response));
+    }));
   }
 
   register(filter: any) {
